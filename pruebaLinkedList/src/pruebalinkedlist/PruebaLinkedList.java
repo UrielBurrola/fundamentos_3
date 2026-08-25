b73583245e0345c64115e9a596c25b9a923fea0b
UrielBurrola <urielburrola25@outlook.com>
2026-08-24 21:11:53 -0700

    Uploaded directory

---
+++ pruebaLinkedList/src/pruebalinkedlist/PruebaLinkedList.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebalinkedlist;
+
+class Student{
+    String name;
+    int score;
+    Student next;
+    
+    public Student(String name, int score){
+        this.name = name;
+        this.score = score;
+        this.next = null;
+    }
+}
+
+class LnkdList {
+    
+    Student head;
+    
+    public LnkdList(){
+        this.head = null;
+    }
+    
+    public void add(String name, int score){
+        Student newStudent = new Student(name,score);
+        if (head == null){
+            head = newStudent;
+        } else {
+            Student current = head;
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = newStudent;
+        }
+    }
+    
+    public void print(){
+        Student current = this.head;
+        while(current !=null) {
+            System.out.println(current.name + " - " + current.score);
+            current = current.next;
+        } 
+    }
+}
+
+
+public class PruebaLinkedList {
+    public static void main(String[] args) {
+        LnkdList lista = new LnkdList();
+        lista.add("Airam",99);
+        lista.add("Sergio", 88);
+        lista.add("Alfedo", 81);
+        System.out.println("agregados.");
+        
+        lista.print();
+    }
+    
+}
